237ef9fcd0939971e025123935fd8bca58ad7ee2
Ordinateur <mds.skali@gmail.com>
2023-03-15 08:55:50 +0100

    firs

---
+++ app/src/main/java/com/example/firstinterface/Mainactivity.java
@@ -0,0 +1,16 @@
+package com.example.firstinterface;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+
+public class Mainactivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view);
+
+    }
+}
